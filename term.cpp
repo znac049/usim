126d331d5f984b34b22adbad32b116c0613e581a
Bob Green <bob@chippers.org.uk>
2024-04-05 14:27:36 +0100

    Added feature so that a host based file can be inserted into the uart input stream. Initialted using the '~<' magic escape sequence.

--- term.cpp
+++ term.cpp
@@ -26,7 +26,9 @@ Terminal::Terminal(USim& sys) : sys(sys)
 {
 	input = stdin;
 	output = stdout;
+	insert = NULL;
 	input_fd = fileno(input);
+	insert_fd = -1;
 
 	// Set input and output to be unbuffered
 	setbuf(input, (char *)0);
@@ -74,13 +76,38 @@ bool Terminal::real_poll_read()
 
 	FD_ZERO(&fds);
 	FD_SET(input_fd, &fds);
+
+	// Are we inserting a host file into the terminal stream?
+	if (insert_fd != -1) {
+		FD_SET(insert_fd, &fds);
+	}
+
 	(void)select(FD_SETSIZE, &fds, NULL, NULL, &tv);
 
-	return FD_ISSET(input_fd, &fds);
+	if (insert_fd != -1) {
+		insert_data_available = FD_ISSET(insert_fd, &fds);
+	}
+
+	return FD_ISSET(input_fd, &fds) || insert_data_available;
 }
 
 Byte Terminal::real_read()
 {
+	if (insert_data_available) {
+		char c = fgetc(insert);
+
+		if (c != EOF) {
+			return c;
+		}
+
+		printf("\r\nEnd of insert file.\r\n");
+
+		fclose(insert);
+		insert = NULL;
+		insert_fd = -1;
+		insert_data_available = false;
+	}
+
 	return fgetc(input);
 }
 
@@ -138,6 +165,7 @@ void Terminal::tilde_escape_help()
 	fprintf(stderr, " ~. - terminate emulator\r\n");
 	fprintf(stderr, " ~? - this message\r\n");
 	fprintf(stderr, " ~~ - send the escape character by typing it twice\r\n");
+	fprintf(stderr, " ~< - insert the contents of a host file into the console stream\r\n");
 	tilde_escape_help_other();
 	fprintf(stderr, "(Note that escapes are only recognized immediately after newline.)\r\n");
 }
@@ -187,6 +215,9 @@ bool Terminal::poll_read()
 				case '?':
 					tilde_escape_help();
 					break;
+				case '<':
+					open_insert_file();
+					break;
 				default:
 					tilde_escape_do_other(ch);
 					break;
@@ -199,6 +230,51 @@ bool Terminal::poll_read()
 	}
 
 	return read_data_available;
+}
+
+bool Terminal::open_insert_file()
+{
+	char filename[255];
+	char *res;
+	int len;
+
+	if (insert_fd != -1) {
+		return false;
+	}
+
+	tcsetattr(input_fd, TCSANOW, &oattr);
+
+	fprintf(stderr, "File: ");
+	res = fgets(filename, sizeof(filename), stdin);
+
+	tcsetattr(input_fd, TCSANOW, &nattr);
+
+	if (res == NULL) {
+		return false;
+	}
+
+	len = strlen(filename);
+	if ((filename[len-1] == '\n') || (filename[len-1] == '\r')) {
+		filename[len-1] = '\0';
+	}
+
+	if ((filename[len-2] == '\n') || (filename[len-2] == '\r')) {
+		filename[len-2] = '\0';
+	}
+
+	// printf("Opening '%s'\n", filename);
+
+	insert = fopen(filename, "r");
+	if (insert != NULL) {
+		insert_fd = fileno(insert);
+		insert_data_available = true;
+
+		// printf("Insert file opened ok. fd# = %d\n", insert_fd);
+
+		return true;
+	}
+
+	return false;
 }
 
 void Terminal::tilde_escape_do_other(char ch)
--- term.h
+++ term.h
@@ -21,6 +21,7 @@ protected:
 	USim&				sys;
 	Byte				read_data;
 	bool				read_data_available = false;
+	bool				insert_data_available = false;
 	int					tilde_escape_phase = 0;
 
 	void				tilde_escape_help();
@@ -28,16 +29,18 @@ protected:
 	virtual void 		tilde_escape_do_other(char ch);
 	bool				real_poll_read();
 	Byte				real_read();
+	bool				open_insert_file();
 
 #ifdef _POSIX_SOURCE
 	FILE*				input;
 	FILE*				output;
+	FILE*				insert;
 	int			 		input_fd;
+	int					insert_fd;
 	struct termios		oattr, nattr;
 #endif // _POSIX_SOURCE
 
 public:
-
 	virtual bool		poll_read();
 	virtual void		write(Byte);
 	virtual Byte		read();
